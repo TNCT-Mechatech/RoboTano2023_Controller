8c653e7fbf3f93d509e93916bb8a1bb4152960b3
Takkun0310 <takkunsan0310@gmail.com>
2024-02-17 17:36:48 +0900

    通信の設定

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 #define SERIAL_RX D4
 
 SerialDev *dev =
-    new MbedHardwareSerial(new BufferedSerial(SERIAL_TX, SERIAL_RX, 9600));
-SerialBridge serial(dev);
+    new MbedHardwareSerial(new BufferedSerial(SERIAL_TX, SERIAL_RX, 115200));
+SerialBridge serial(dev,1024);
 
 AnalogIn X(A6);         //平行移動用のジョイスティックの左右
 AnalogIn Y(A5);         //平行移動用のジョイスティックの上下
@@ -30,17 +30,18 @@ int main() {
   serial.add_frame(0, &msg);
 
   while (true) {
-    msg.data.joystick_x = static_cast<int8_t>((X.read() - 0.5) * 200);
-    msg.data.joystick_y = static_cast<int8_t>((Y.read() - 0.5) * 200);
-    msg.data.joystick_turn = static_cast<int8_t>((Turn.read() - 0.5) * 200);
-    msg.data.moter_speed = static_cast<int8_t>((Speed.read() - 0.5) * 200);
+    msg.data.joystick_x = static_cast<int8_t>((X.read() - 0.5) * 200); //x方向のジョイスティックの値を1byteで送信できるように変換かつ値の範囲を-100~100までにする
+    msg.data.joystick_y = static_cast<int8_t>((Y.read() - 0.5) * 200);//Y方向のジョイスティックの値を1byteで送信できるように変換かつ値の範囲をを-100~100までにする
+    msg.data.joystick_turn = static_cast<int8_t>((Turn.read() - 0.5) * 200);//旋回用のジョイスティックの値を1byteで送信できるように変換かつ値の範囲を-100~100までにする
+    msg.data.moter_speed = static_cast<int8_t>((Speed.read() - 0.5) * 200);//ローラーの回転速度の値を1byteで送信できるように変換かつ値の範囲を-100~100までにする
     msg.data.roller_status = (Roller == 1) ? true : false;
     msg.data.shoot_bottom = (Shoot == 1) ? true : false;
     msg.data.arm_up = (Arm_up == 1) ? true : false;
     msg.data.arm_down = (Arm_down == 1) ? true : false;
     msg.data.hand_status = (Hand == 1) ? true : false;
     serial.write(0);
-    wait_us(100 * 1000);
-    printf("左右:%d\n", msg.data.moter_speed);
+    printf("抵抗値:%d  " ,msg.data.moter_speed);
+    printf("スイッチ:%s\n",msg.data.roller_status ? "true" : "false");
+    wait_us(70 * 1000);
   }
 }
